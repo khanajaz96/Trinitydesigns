eb6899f8162960779097274033237363b22a58e5
khanajaz96 <52720034+khanajaz96@users.noreply.github.com>
2019-12-05 04:45:31 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// COUNTER
+// Counter
+$('.counter').counterUp({
+  delay: 10,
+  time: 2000
+});
+$('.counter').addClass('animated fadeInDownBig');
+$('h3').addClass('animated fadeIn');
+// Testimonials
